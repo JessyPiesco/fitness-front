60f427ead6cfea8a9e1aab8f1bac3ae83434a0d1
minimyl <aandrew.myles@gmail.com>
2022-11-15 13:25:55 -0500

    adding useState into login/register

--- src/apiFunctions/index.js
+++ src/apiFunctions/index.js
@@ -1,29 +1,50 @@
-const BASE = "http://fitnesstrac-kr.herokuapp.com/api"
+const BASE = "http://fitnesstrac-kr.herokuapp.com/api";
 
-export async function getRoutines(){
-  const response= await fetch(`${BASE}/routines`);
+export async function getRoutines() {
+  const response = await fetch(`${BASE}/routines`);
   const result = await response.json();
 
   return result;
 }
 
 export async function loginUser(username, password) {
-    try{
-        const loginResult = await fetch(`${BASE_URL}/users/login`,{
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        }).then(response => response.json())
-          .then(result => {
-            console.log(result);
-          });
+  try {
+    const loginResult = await fetch(`${BASE}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password
+      }),
+    });
+    const resp = await loginResult.json();
+    console.log(resp)
+    return resp.token;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function registerUser(username, password) {
+    const options = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        }, body: JSON.stringify({
+            username, 
+            password
+        })
     }
-    catch(error){
-        console.error(error)
+    const response = await fetch(`${BASE}/users/register`,options)
+    const result = await response.json()
+
+    console.log(response)
+    console.log(result)
+
+    if (result.error){
+        alert ("Account is already taken. Please choose a different Username")
     }
+    return result.token
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { loginUser } from "../apiFunctions";
 
 
-const Login = () => {
+const Login = (props) => {
+    const [userName, setUserName]= useState("")
+
+
+    async function handleSubmit(e) {
+        try {
+            e.preventDefault();
+            const username = e.target[0].value;
+            const password = e.target[1].value;
+            const token = await loginUser(username, password)
+
+            localStorage.removeItem("token");
+            localStorage.setItem("token", token);
+            setUserName(userName)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
     return (
         <div className="login">
             <h2>Log in</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>Username</label>
                     <input type="text"/>
