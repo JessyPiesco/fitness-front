cc3fc006099721b2a7de6227a29ea88129229fde
Jessy <japarker0421@gmail.com>
2022-11-15 08:18:31 -0700

    added base URL

---
+++ src/components/Routines.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from "react";
+
+const Routines = () => {
+  return (
+    <div id="routines">
+      <h2> I am Routine</h2>
+    </div>
+  );
+};
+
+export default Routines;
