e52b221a924feba3086fbad132e63bfeede849d9
minimyl <aandrew.myles@gmail.com>
2022-11-15 19:42:17 -0500

    css for login and register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,55 @@
 import React, { useState } from "react";
 import { loginUser } from "../apiFunctions";
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 
 const Login = (props) => {
-    const [userName, setUserName]= useState("")
-    const setLoggedIn = props.setLoggedIn
-    const loggedIn = props.loggedIn
-    const getLoggedInUser = props.getLoggedInUser
+  const [userName, setUserName] = useState("");
+  const setLoggedIn = props.setLoggedIn;
+  const loggedIn = props.loggedIn;
+  const getLoggedInUser = props.getLoggedInUser;
 
-    async function handleSubmit(e) {
-        try {
-            e.preventDefault();
-            const username = e.target[0].value;
-            const password = e.target[1].value;
-            const token = await loginUser(username, password)
-            
-            localStorage.removeItem("token");
-            localStorage.setItem("token", token);
-            setUserName(userName)
-            setLoggedIn(true)
-            getLoggedInUser()
-            
-        } catch (error) {
-            console.log(error)
-        }
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const username = e.target[0].value;
+      const password = e.target[1].value;
+      const token = await loginUser(username, password);
+
+      localStorage.removeItem("token");
+      localStorage.setItem("token", token);
+      setUserName(userName);
+      setLoggedIn(true);
+      getLoggedInUser();
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  return (
+    <div id="login-register">
+      <div className="box">
+        <h2 className="inputH">Log in</h2>
+        <form className="dataInput" onSubmit={handleSubmit}>
+          <div className="userBox">
+            <input className="userInput" type="text" placeholder="username" />
+          </div>
+          <div className="passBox">
+            <input
+              className="userInput"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <button className="submitBtn" type="submit">
+            Login
+          </button>
+        </form>
+        <NavLink className="signBtn" to="/register">
+          New User Sign Up
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="box">
-            <h2>Log in</h2>
-            <form className="dataInput" onSubmit={handleSubmit}>
-                <div className="userBox">
-                    <input className="userInput" type="text" placeholder="username"/>
-                </div>
-                <div className="passBox">
-                    <input className="userInput" type="password" placeholder="password"/>
-                </div>
-                <button className="submitBtn" type="submit">
-                    Login        
-                </button>
-            </form>
-            <NavLink className="signBtn" to="/register">New User Sign Up</NavLink>
-        </div>
-    )
-
-}
-
-export default Login
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,48 +1,51 @@
-import React from "react"
+import React from "react";
 import { registerUser } from "../apiFunctions";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    async function handleRegister(e) {
-        try {
-            e.preventDefault();
-            const username = e.target[0].value;
-            const password = e.target[1].value;
-            const token = await registerUser(username, password)
+  async function handleRegister(e) {
+    try {
+      e.preventDefault();
+      const username = e.target[0].value;
+      const password = e.target[1].value;
+      const token = await registerUser(username, password);
 
-            localStorage.removeItem("token");
-            localStorage.setItem("token", token);
-            
-            if(token){
-                navigate("/login");
-                alert("Thank you for Signing Up, please Log In")
-            } 
+      localStorage.removeItem("token");
+      localStorage.setItem("token", token);
 
-;        } catch (error) {
-            console.log(error)
-        }
+      if (token) {
+        navigate("/login");
+        alert("Thank you for Signing Up, please Log In");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  return (
+    <div id="login-register">
+      <div className="box">
+        <h2 className="inputH">Register</h2>
+        <form className="dataInput" onSubmit={handleRegister}>
+          <div className="userBox">
+            <input className="userInput" type="text" placeholder="username" />
+          </div>
+          <div className="passBox">
+            <input
+              className="userInput"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <button className="submitBtn" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="box">
-            <h2>Register</h2>
-            <form className="dataInput" onSubmit={handleRegister}>
-                <div className="userBox">
-                    <input className="userInput" type="text" placeholder="username"/>
-                </div>
-                <div className="passBox">
-                    <input className="userInput" type="password" placeholder="password"/>
-                </div>
-                <button className="submitBtn" type="submit">
-                    Register
-                </button>
-            </form>
-        </div>
-    )
-
-}
-
-export default Register
+export default Register;
